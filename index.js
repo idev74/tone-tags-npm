ed5b32d7260df9a50254588b8228e9b308b70176
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2023-06-30 16:28:32 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const prompt = require('prompt-sync')();
+const tags = {
+    "joking": "/j",
+    "half-joking": "/hj",
+    "serious": "/srs",
+    "light-hearted": "/lh",
+    "genuine": "/gen",
+    "genuine question": "/genq",
+    "positive connotation": "/pos",
+    "negative connotation": "/neg",
+    "neutral connotation": "/neu",
+    "not mad": "/nm",
+    "platonic": "/p",
+    "romantic": "/r",
+    "copypasta": "/c",
+    "lyrics": "/ly",
+    "hyperbole": "/hyp",
+}
+

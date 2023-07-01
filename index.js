469c005dc46292679bc99528f841850baf30cb9f
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2023-07-01 11:57:24 -0700

    complete first iteration

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
-const prompt = require('prompt-sync')();
 const tags = {
     "joking": "/j",
     "half-joking": "/hj",
     "serious": "/srs",
+    "sarcastic": "/s",
     "light-hearted": "/lh",
-    "genuine": "/gen",
     "genuine question": "/genq",
-    "positive connotation": "/pos",
-    "negative connotation": "/neg",
-    "neutral connotation": "/neu",
+    "genuine": "/gen",
+    "positive": "/pos",
+    "negative": "/neg",
+    "neutral": "/neu",
     "not mad": "/nm",
     "platonic": "/p",
     "romantic": "/r",
     "copypasta": "/c",
     "lyrics": "/ly",
     "hyperbole": "/hyp",
+    "metaphor": "/m",
+    "rhetorical": "/rh",
+}
+
+function tagify(text) {
+    let newText = text;
+    Object.keys(tags).forEach(tag => {
+        const tagRegex = new RegExp(`\\(${tag}\\)`, "g");
+        newText = newText.replace(tagRegex, tags[tag]);
+    });
+    return newText;
+}
+
+function decode(text) {
+    let newText = text;
+    Object.keys(tags).forEach(tag => {
+        decodeRegex = new RegExp(tags[tag], "gi");
+        newText = newText.replace(decodeRegex, `(${tag})`);
+    });
+    return newText;
+}
+
+module.exports = {
+    tagify,
+    decode
 }
 
